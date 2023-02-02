c89e23bd5e517c36d866981fa9edac4980d91b2a
willem-strydom <s.willem@wustl.edu>
2023-02-02 09:42:49 -0600

    final commit?

--- fake_records.py
+++ fake_records.py
@@ -2,10 +2,11 @@
 # of 1000 records with the following
 # fields: First Name, Last Name, Birthday, Email, and Phone Number
 
-import Faker
+import faker
 import pandas as pd
+import numpy as np
 
-fake = Faker()
+fake = faker()
 
 
 def generate():
@@ -16,4 +17,25 @@ def generate():
     pnum = [fake.phone_number() for i in range(1000)]
     df = pd.DataFrame({'First Name': namesf, 'Last Names': namesl, 'Birthday': bday, 'Email': email,
                        'Phone Number': pnum})
+    return df
+def save():
+    df = generate()
+    file_path = "desktop/cse314/git-github-fundamentals-willem-strydom/.github/data"
+    with open(file_path, 'w', newline='') as file:
+        df.to_csv("fake_records.csv", index=False)
+
+def assign_salaries(df):
+    salaries = [np.random.randint(20000,100000) for i in range(1000)]
+    df = df.assign(Salary = salaries)
+    return df
+
+def over_50k(df):
+    df = df[df['Salary']>50000]
+    return df
+
+def normalize(series):
+    return (series - series.mean()) / series.std()
+
+def assign_normalized_salaries(df):
+    df = df.assign(Normalized_Salaries = normalize(df['Salary']))
     return df
